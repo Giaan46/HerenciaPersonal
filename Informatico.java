2cccb94e7afdefcb9ca7f37c3ff724495c4631ff
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-14 12:08:23 +0100

    Add files via upload

---
+++ Informatico.java
@@ -0,0 +1,12 @@
+package sentencias;
+
+public class Informatico extends PAS{
+	
+	// Atributos
+
+    public Informatico(String nombre, String dni, int horasExtra) {
+		// Rellena el constructor
+		super(nombre, dni, 40, 6.0, horasExtra); // horasXContrato --> 40   pagaXHora --> 6.0
+	}
+    
+}
