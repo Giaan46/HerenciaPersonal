9bc32d071f2ed2ba865c32ebd0f051310f376703
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-14 12:07:38 +0100

    Add files via upload

---
+++ PDI.java
@@ -0,0 +1,15 @@
+package sentencias;
+
+public class PDI extends Personal {
+
+	// Atributos
+
+	// Constructor
+
+	PDI(String nombre, String dni, double horasXContrato, double pagaXHora) {
+		super(nombre, dni, horasXContrato, pagaXHora) ;
+	}
+
+	// Metodos
+
+}
