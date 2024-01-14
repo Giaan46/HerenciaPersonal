d67344969ecb973ab707249a65e316f603d2ed42
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-01-14 12:08:34 +0100

    Add files via upload

---
+++ Administrativo.java
@@ -0,0 +1,13 @@
+package sentencias;
+
+public class Administrativo extends PAS{
+	 
+	// Atributos
+
+    public Administrativo(String nombre, String dni, int horasExtra) {
+		// Rellena el constructor
+		super(nombre, dni, 37.0, 7.5, horasExtra); // horasXContrato --> 37.0   pagaXHora --> 7.5
+	}
+    
+
+}
